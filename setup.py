840bc46778f86fba56289fe23bb0b5416175d020
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2025-04-24 12:41:00 +0200

    Overhaul
    1) Package for PyPI. 2) Enforce all alerts. 3) Drop CLI. 4) Change name. 5) Update README.

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
 
+"""Setup module."""
 
-setup(
-    name="rd_filters",
-    version="0.1",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'rd_filters=rd_filters.rd_filters:main',
-        ],
-    },
-    install_requires=['pandas', 'docopt'],
-    include_package_data=True,
-)
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup()
